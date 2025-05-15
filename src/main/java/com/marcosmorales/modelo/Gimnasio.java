a23bbe6baa2c1d55685502b1f8816b8fcacf6e04
ramiromarcosmorales <marianamorales62@yahoo.com>
2025-05-15 09:23:54 +0200

    feat: implementar tads

--- src/main/java/com/marcosmorales/modelo/Gimnasio.java
+++ src/main/java/com/marcosmorales/modelo/Gimnasio.java
@@ -3,74 +3,67 @@ package com.marcosmorales.modelo;
 import java.time.LocalDate;
 import java.time.LocalTime;
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
+import java.util.PriorityQueue;
 
 public class Gimnasio {
     private List<Empleado> empleados = new ArrayList<>();
     private List<Socio> socios = new ArrayList<>();
-    private List<Clase> clases = new ArrayList<>();
-    private List<Maquina> maquinas = new ArrayList<>();
+    private List<Clase> clases = new ArrayList<>(); // reemplazar por tad
+    private PriorityQueue<Maquina> maquinasPriority;
 
+    public Gimnasio() {
+        this.maquinasPriority = new PriorityQueue<>(
+                Comparator.comparing(Maquina::getProxMantenimiento)
+        );
+    }
 
     public boolean contratarEmpleado(Empleado empleado) {
-        if (empleado == null) {
-            throw new IllegalArgumentException("El empleado no puede ser nulo!");
-        }
+        if (empleado == null) throw new IllegalArgumentException("El empleado no puede ser nulo!");
 
-        if (empleados.contains(empleado)) {
-            return false;
-        }
 
-        empleados.add(empleado);
-        return true;
+        if (empleados.contains(empleado)) return false;
+
+        return empleados.add(empleado);
     }
 
     public boolean registrarSocio(Socio socio) {
-        if (socio == null) {
-            throw new IllegalArgumentException("El socio no puede ser nulo!");
-        }
+        if (socio == null) throw new IllegalArgumentException("El socio no puede ser nulo!");
 
-        if (socios.contains(socio)) {
-            return false;
-        }
 
-        socios.add(socio);
-        return true;
+        if (socios.contains(socio)) return false;
+
+        return socios.add(socio);
     }
 
     public boolean registrarMaquina(Maquina maquina) {
-        if (maquina == null) {
-            throw new IllegalArgumentException("La maquina no puede ser nulo!");
-        }
+        if (maquina == null) throw new IllegalArgumentException("La maquina no puede ser nulo!");
 
-        if (maquinas.contains(maquina)) {
-            return false;
-        }
+        if (maquinasPriority.contains(maquina)) return false;
 
-        maquinas.add(maquina);
-        return true;
+        return maquinasPriority.add(maquina);
+    }
+
+    public Maquina getProximaMaquinaMantenimiento() {
+        return maquinasPriority.peek();
     }
 
     public boolean reservarClase(Socio socio, LocalDate fecha, LocalTime horario) {
-        if (socio == null || fecha == null || horario == null) {
-            throw new IllegalArgumentException("Uno de los datos recibidos es nulo!");
-        }
+        if (socio == null || fecha == null || horario == null) throw new IllegalArgumentException("Uno de los datos recibidos es nulo!");
 
-        if (!socio.getCarnet().verificarValidez()) {
-            throw new IllegalStateException("El carnet del socio no está activo!");
-        }
+
+        if (!socio.getCarnet().verificarValidez()) throw new IllegalStateException("El carnet del socio no está activo!");
+
 
         for (Clase clase : clases) {
-            if (clase.getFecha().equals(fecha) && clase.getHorario().equals(horario)) {
-                throw new IllegalStateException("Ya existe una clase reservada en esta fecha y horario!");
-            }
+            if (clase.getFecha().equals(fecha) && clase.getHorario().equals(horario)) throw new IllegalStateException("Ya existe una clase reservada en esta fecha y horario!");
         }
 
         Clase clase = Clase.crearClase(fecha, horario);
 
         if (socio.getClases().add(clase)) {
-            clases.add(clase);
-            return true;
+            return clases.add(clase);
         } else {
             return false;
         }
--- src/main/java/com/marcosmorales/modelo/Socio.java
+++ src/main/java/com/marcosmorales/modelo/Socio.java
@@ -1,27 +1,26 @@
 package com.marcosmorales.modelo;
 
 import java.time.LocalDate;
-import java.time.LocalDateTime;
 import java.time.LocalTime;
-import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
 
 public class Socio extends Usuario implements Registrable {
     private Carnet carnet;
-    private List<Clase> clases;
+    private LinkedList<Clase> clases;
 
     // Constructor sin parámetros
     public Socio() {
         super();
         this.carnet = null;
-        this.clases = new ArrayList<>();
+        this.clases = new LinkedList<>();
     }
 
     // Constructor con parámetros
     private Socio(String nombre, String apellido, String email, String telefono) {
         super(nombre, apellido, email, telefono);
         this.carnet = Carnet.generarCarnet();
-        this.clases = new ArrayList();
+        this.clases = new LinkedList<>();
     }
 
     // Getters
@@ -39,14 +38,11 @@ public class Socio extends Usuario implements Registrable {
     }
 
     public void reservarClase(LocalDate fecha, LocalTime horario) {
-        if (!carnet.verificarValidez()) {
-            throw new IllegalStateException("Membresia Ináctiva!");
-        }
+        if (!carnet.verificarValidez()) throw new IllegalStateException("Membresia Ináctiva!");
 
         for (Clase clase : clases) {
-            if (clase.getFecha().equals(fecha) && clase.getHorario().equals(horario)) {
-                throw new IllegalStateException("Ya existe una clase reservada en esta fecha y horario!");
-            }
+            if (clase.getFecha().equals(fecha) && clase.getHorario().equals(horario)) throw new IllegalStateException("Ya existe una clase reservada en esta fecha y horario!");
+
         }
 
         clases.add(Clase.crearClase(fecha, horario));
